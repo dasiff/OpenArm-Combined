56af3d90458a4171c4a85515e2864599a932db7b
rkothari3 <rajg6.isb@gmail.com>
2026-02-27 13:24:32 -0500

    refactor

--- view_xml.py
+++ view_xml.py
@@ -3,7 +3,7 @@ import mujoco.viewer
 import time
 import sys
 
-xml_path = sys.argv[1] if len(sys.argv) > 1 else "tracer2.xml"
+xml_path = sys.argv[1] if len(sys.argv) > 1 else "Tracer.xml"
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
 
